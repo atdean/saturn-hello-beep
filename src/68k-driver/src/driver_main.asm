2a32cb7cc021e116e0cd55a999ca9741bee36751
Amber Dean <amberdean89@gmail.com>
2021-08-20 06:29:01 -0400

    Fixed minor syntax issue. Build now works again.

--- src/68k-driver/src/driver_main.asm
+++ src/68k-driver/src/driver_main.asm
@@ -7,12 +7,12 @@ init_scsp:
     ; TODO :: 68K must wait approx 30µs after SMPC reset for SCSP to init registers
 
     ; Configure SCSP for 4Mbit sound RAM, and 16-bit DAC output (retail Saturn hardware)
-    move.l #reg_SCSP_M4D8, a0
-    move.b #%00000010, (a0)
+    move.l #reg_SCSP_M4D8,a0
+    move.b #%00000010,(a0)
 
     ; Set SCSP master volume to -0db (no attenuation)
-    move.l #reg_SCSP_MVOL, a0
-    move.b #$0F, (a0)
+    move.l #reg_SCSP_MVOL,a0
+    move.b #$0F,(a0)
 
 driver_main_loop:
     jmp    run_idle_loop ; for now, just idle until I start implementing functionality
